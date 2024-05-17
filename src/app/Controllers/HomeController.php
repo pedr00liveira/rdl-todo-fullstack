743d53c05a70a4a9570ae8a6eb33302c7cbe92b5
pedr00liveira <pedroco06@gmail.com>
2024-05-17 19:48:11 +0200

    Bug corrections and new functions

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -54,9 +54,20 @@ class HomeController extends Controller {
                     ];
                 }
 
-                $sql = "SELECT name, email, icon
-                        FROM user
-                        WHERE id_user = ". $_SESSION['user'];
+                $sql = "SELECT 
+                            u.name, 
+                            u.email, 
+                            u.icon, 
+                            (SELECT COUNT(t.id_task) 
+                            FROM task t 
+                            WHERE t.ref_id_user = u.id_user) AS tasks, 
+                            (SELECT COUNT(tt.id_type_task) 
+                            FROM type_task tt 
+                            WHERE tt.ref_id_user = u.id_user) AS cats
+                        FROM 
+                            user u
+                        WHERE 
+                            u.id_user = " . $_SESSION['user']; 
 
                 $result = $this->container->db->prepare($sql);
 
@@ -68,8 +79,30 @@ class HomeController extends Controller {
                         $json['user'] = [
                             'name' => $row['name'],
                             'email' => $row['email'],
-                            'icon' => $row['icon']
+                            'icon' => $row['icon'],
+                            'tasks' => $row['tasks'],
+                            'categories' => $row['cats']
                         ];
+                    }
+
+                    $sql = "SELECT id_icon, class
+                            FROM icon";
+
+                    $result = $this->container->db->prepare($sql);
+                    
+                    if ($result->execute()) {
+
+                        $res = $result->fetchAll();
+
+                        $json['icon'] = array();
+
+                        foreach ($res as $row) {
+                            
+                            $json['icon'][] = [
+                                'id' => $row['id_icon'],
+                                'class' => $row['class']
+                            ];
+                        }
                     }
 
                     return $json;
@@ -91,7 +124,41 @@ class HomeController extends Controller {
     }
 
     public function renderCalendar($request, $response) {
-        return $this->container->view->render($response, 'calendar.phtml', ['response' => HomeController::renderSideBar()]);
+        $json = HomeController::renderSideBar();
+
+        $sql = "SELECT t.id_task as id, t.name as task, tt.id_type_task as id_type, tt.name as type, i.class as icon, t.until
+                    FROM task t
+                    INNER JOIN type_task tt
+                    ON t.ref_id_type = tt.id_type_task
+                    INNER JOIN icon i
+                    ON t.ref_id_icon = i.id_icon
+                    WHERE t.ref_id_user = ". $_SESSION['user'] . 
+                    " ORDER BY until";
+
+        $result = $this->container->db->prepare($sql);
+
+        if ($result->execute()) {
+
+            $res = $result->fetchAll();
+
+            $json['calendar'] = array();
+            
+            foreach ($res as $row) {
+                $json['calendar'][] = [
+                    'category' => $row['id_type'],
+                    'name_category' => $row['type'],
+                    'id' => $row['id'],
+                    'name' => $row['task'],
+                    'deadline' => $row['until'],
+                    'icon' => $row['icon']
+                ];
+            }
+
+            return $this->container->view->render($response, 'calendar.phtml', ['response' => $json]);
+        }
+
+        return false;
+        
     }
 
     public function renderCreateTask($request, $response) {
@@ -107,14 +174,16 @@ class HomeController extends Controller {
         $title = $request->getParam('title');
         $desc = $request->getParam('desc');
         $category = $request->getParam('category');
+        $deadline = $request->getParam('until');
+        $icon = $request->getParam('icon');
 
-        $sql = "INSERT INTO task (nome, email, pass)
-                VALUES ('$title', '$desc', '$category')";
+        $sql = "INSERT INTO task (name, description, ref_id_type, until, ref_id_user, ref_id_icon)
+                VALUES ('$title', '$desc', '$category', '$deadline', ". $_SESSION['user'] .", $icon)";
 
         $result = $this->container->db->prepare($sql);
 
         if ($result->execute()) {
-            return $response->withRedirect('signin');
+            return $response->withRedirect('task');
         }
 
         echo 'error';
@@ -124,31 +193,150 @@ class HomeController extends Controller {
     public function createCategory($request, $response) {
         
         $title = $request->getParam('title');
-        $desc = $request->getParam('desc');
-        $category = $request->getParam('category');
 
-        $sql = "INSERT INTO task (nome, email, pass)
-                VALUES ('$title', '$desc', '$category')";
+        $sql = "INSERT INTO type_task (name, ref_id_user)
+                VALUES ('$title', '". $_SESSION['user'] ."')";
 
         $result = $this->container->db->prepare($sql);
 
         if ($result->execute()) {
-            return $response->withRedirect('signin');
+            return $response->withRedirect('../calendar');
         }
 
         echo 'error';
         return false;
     }
 
-    public function renderInfo($request, $response) {
-        return $this->container->view->render($response, 'info.html');
+    public function renderInfo($request, $response, $args) {
+        
+        $sql = "SELECT t.id_task as id, t.name as task, t.description as description, tt.id_type_task as id_type, tt.name as type, t.until as deadline
+                FROM task t
+                INNER JOIN type_task tt
+                ON t.ref_id_type = tt.id_type_task
+                INNER JOIN icon i
+                ON t.ref_id_icon = i.id_icon
+                WHERE t.id_task = " . $args['id'];
+
+        $result = $this->container->db->prepare($sql);
+
+        if ($result->execute()) {
+
+            $res = $result->fetchAll();
+            
+            foreach ($res as $row) {
+                $json['task'] = [
+                    'category' => $row['id_type'],
+                    'name_category' => $row['type'],
+                    'id' => $row['id'],
+                    'name' => $row['task'],
+                    'desc' => $row['description'],
+                    'deadline' => $row['deadline']
+                ];
+            }
+        }
+
+        return $this->container->view->render($response, 'info.phtml', ['response' => $json]);
     }
     
-    public function renderEdit($request, $response) {
-        return $this->container->view->render($response, 'edit.html');
+    public function renderEdit($request, $response, $args) {
+        
+        $sql = "SELECT t.id_task as id, t.name as task, t.description as description, tt.id_type_task as id_type, tt.name as type, t.until as deadline, t.ref_id_icon as icon
+        FROM task t
+        INNER JOIN type_task tt
+        ON t.ref_id_type = tt.id_type_task
+        INNER JOIN icon i
+        ON t.ref_id_icon = i.id_icon
+        WHERE t.id_task = " . $args['id'];
+
+        $result = $this->container->db->prepare($sql);
+
+        if ($result->execute()) {
+
+            $res = $result->fetchAll();
+            
+            foreach ($res as $row) {
+                $json['task'] = [
+                    'category' => $row['id_type'],
+                    'name_category' => $row['type'],
+                    'id' => $row['id'],
+                    'name' => $row['task'],
+                    'desc' => $row['description'],
+                    'deadline' => $row['deadline'],
+                    'icon' => $row['icon']
+                ];
+            }
+
+            $sql = "SELECT id_type_task, name
+                FROM type_task
+                WHERE ref_id_user = ". $_SESSION['user'];
+
+            $result = $this->container->db->prepare($sql);
+            
+            if ($result->execute()) {
+
+                $res = $result->fetchAll();
+
+                $json['category'] = array();
+
+                foreach ($res as $row) {
+                    
+                    $json['category'][] = [
+                        'id' => $row['id_type_task'],
+                        'name' => $row['name']
+                    ];
+                }
+
+                $sql = "SELECT id_icon, class
+                            FROM icon";
+
+                $result = $this->container->db->prepare($sql);
+                
+                if ($result->execute()) {
+
+                    $res = $result->fetchAll();
+
+                    $json['icon'] = array();
+
+                    foreach ($res as $row) {
+                        
+                        $json['icon'][] = [
+                            'id' => $row['id_icon'],
+                            'class' => $row['class']
+                        ];
+                    }
+
+                    return $this->container->view->render($response, 'edit.phtml', ['response' => $json]);
+                }
+            }
+        }
+
+        return false;
+        
     }
 
-    public function edit($request, $response) {
-        return $this->container->view->render($response, 'edit.html');
+    public function edit($request, $response, $args) {
+
+        $title = $request->getParam('title');
+        $desc = $request->getParam('desc');
+        $category = $request->getParam('category');
+        $deadline = $request->getParam('until');
+        $icon = $request->getParam('icon');
+
+        $sql = "UPDATE task
+                SET name = '$title', ref_id_type = $category, description = '$desc', until = '$deadline', ref_id_icon = '$icon'
+                WHERE id_task = " . $args['id'];
+        $result = $this->container->db->prepare($sql);
+
+        if ($result->execute()) {
+            return $response->withRedirect('info/' . $args['id']);
+        }
+
+        echo 'error';
+        return false;
+        
+    }
+
+    public function renderProfile($request, $response) {
+        return $this->container->view->render($response, 'profile.phtml', ['response' => HomeController::renderSideBar()]);
     }
 }
--- src/app/routes.php
+++ src/app/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-$app->post('/getCategory', 'HomeController:renderCategory')->setName('render.category');
+$app->post('/getCategory', 'HomeController:renderCategory');
 
 $app->get('/home', 'HomeController:redirectCalendar');
 
@@ -14,10 +14,14 @@ $app->post('/home/new/task', 'HomeController:createTask');
 $app->get('/home/new/category', 'HomeController:renderCreateCategory');
 $app->post('/home/new/category', 'HomeController:createCategory');
 
-$app->get('/home/info', 'HomeController:renderInfo');
+$app->get('/home/info/{id}', 'HomeController:renderInfo');
 
-$app->get('/home/edit', 'HomeController:renderEdit');
-$app->post('/home/edit', 'HomeController:edit');
+$app->get('/home/edit/{id}', 'HomeController:renderEdit');
+$app->post('/home/edit/{id}', 'HomeController:edit');
+
+
+$app->get('/home/profile', 'HomeController:renderProfile');
+// $app->post('/home/profile', 'HomeController:edit');
 
 $app->get('/auth/signup', 'AuthController:renderSignUp');
 $app->post('/auth/signup', 'AuthController:signUp');
